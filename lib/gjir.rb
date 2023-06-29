93eb4d86c058c3ae05118c4911c99f01203f3bd9
Genki Takiuchi <genki@s21g.com>
2023-06-30 02:46:28 +0900

    First commit

---
+++ lib/gjir.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "gjir/version"
+
+PROC_NAME = "GoogleJapaneseInput"
+
+# find pid of GoogleJapaneseInput
+pid = `ps aux | grep #{PROC_NAME}\$ | grep -v grep | awk '{print $2}'`.chomp
+
+# kill the process
+puts "Killing #{PROC_NAME} (pid:#{pid}) process..."
+`kill -9 #{pid}` unless pid.empty?
